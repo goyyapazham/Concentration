2a39e332983646ab25a226f51a2e46557657f3a6
goyyapazham <nsharadjaya@stuy.edu>
2015-11-23 19:16:17 -0500

    Update Concentration.java
    updated play()

--- Concentration.java
+++ Concentration.java
@@ -13,41 +13,93 @@ public class Concentration {
     //insert constructor and methods here
 
     public Concentration() {
+	
 	_numberFaceUp = 0;
 	_words = new String[] { "a", "b", "c", "d", "e", "f", "g", "h" };
 	_board = setBoard();
+	
     }
 
     public Tile[][] setBoard() {
-	int index = 0;
 	for(int r = 0; r < 4; r++) {
 	    for(int c = 0; c < 4; c++) {
-		Tile t = new Tile (_words[index]);
+		Tile t = new Tile (_words[(4 * r + c) % 8]);
 		_board[r][c] = t ;
-		index ++;
-	        if (index == 8){
-		    index = 0;}
 	    }
 	}
-	
-
 	
 	for(int r = 0; r < 4; r++) {
 	    for(int c = 0; c < 4; c++) {
-	    int rand1 = (int)(Math.random() * 4);
-	    int rand2 = (int)(Math.random() * 4);
-	    swap(_board, r, c, rand1, rand2);
+		int rand1 = (int)(Math.random() * 4);
+		int rand2 = (int)(Math.random() * 4);
+		swap(_board, r, c, rand1, rand2);
 	    }
 	}
-
+	
 	return _board;
 
     }
 
     public static void swap(Object[][] a, int r1, int r2, int c1, int c2) {
-	Object holder = a[r1][c1];
-        a[r1][c1] = a[r2][c2];
-        a[r2][c2] = holder;      
+	Object o1 = a[r1][c1];
+	Object o2 = a[r2][c2];
+	a[r1][c1] = o2;
+	a[r2][c2] = o1;
+    }
+
+    public static int getSomething(String query) {
+	String error = "That wasn't a digit between 1-4. Please try again.";
+	System.out.println(query);
+	int i = Keyboard.readInt() - 1;
+	if (i < 0 || i > 3) {
+	    System.out.println(error);
+	    return getSomething(query);
+	}
+	return i;
+    }
+
+    public boolean allUp() {
+	for(int r = 0; r < 4; r++) {
+	    for(int c = 0; c < 4; c++) {
+		if (!_board[r][c].isFaceUp()) return false;
+	    }
+	}
+	return true;
+    }
+
+    public String play() {
+	int r1 = getSomething("First row (digit between 1-4):");
+	int c1 = getSomething("First column (digit between 1-4):");
+	int r2 = getSomething("Second row (digit between 1-4):");
+	int c2 = getSomething("Second column (digit between 1-4):");
+	if ( r1 == r2 && c1 == c2 ) {
+	    System.out.println("You flipped the same cards! Try again:");
+	    play();
+	}
+        Tile t1 = _board[r1][c1];
+	Tile t2 = _board[r2][c2];
+	if ( t1.isFaceUp() || t2.isFaceUp() ) {
+	    System.out.println("One or more of those cards was already flipped up! Try again:");
+	    play();
+	}
+	t1.flip();
+	t2.flip();
+        while (!allUp()) {
+	    if (t1.equals(t2)) {
+		System.out.print(toString());
+		System.out.println("That's a match! Try again:");
+		play();
+	    }
+	    else {
+		System.out.print(toString());
+		System.out.println("That wasn't a match! Try again:");
+		t1.flip();
+		t2.flip();
+		play();
+	    }
+	}
+	return "You won! Great job.";
+	
     }
 
     public String toString() {
@@ -61,86 +113,12 @@ public class Concentration {
 	return retBoard;
     }
 
-    public String ready (){
-	String response = Keyboard.readString();
-	if (response.equals("Y")){
-	    return ("Then let's begin.\n");}
-	else{
-	    System.out.println ("Take your time...");
-		return ready ();}}
-	
-	
-
-    public void play(){
-	int score = 0;
-	int number = 0;
-	System.out.println ("Welcome to Concetration!");
-	System.out.println ("Please observe the letters below.");
-	for(int r = 0; r < 4; r++) {
-	    for(int c = 0; c < 4; c++) {
-		this._board[r][c].flip();}}
-	System.out.print (this);
-	System.out.println ("Got it memorized?(Y/N)");
-	System.out.print (ready ());
-        for(int r = 0; r < 4; r++) {
-	   for(int c = 0; c < 4; c++) {
-	       this._board[r][c].flip();}}
-	System.out.print (this);
-	while (score < 8){
-
-	     System.out.println ("\nPlease indicate the row of the first element.");
-	    int rowA = Keyboard.readInt();
-	    while (rowA > ( _board.length) || rowA <= 0){
-	          System.out.println ("That row does not exist. Try again.");
-	          rowA = Keyboard.readInt();}
-	          
-	    System.out.println ("\nPlease indicate the column of the first element.");
-	    int columnA = Keyboard.readInt();
-	    while (columnA > ( _board[0].length) || columnA <=0 ){
-	          System.out.println ("That column does not exist. Try again.");
-	          columnA = Keyboard.readInt();}
-	    _board[rowA-1][columnA-1].flip();
-	    System.out.print(this);
-	          
-	    System.out.println ("\nPlease indicate the row of the second element.");
-	    int rowB = Keyboard.readInt();
-	    while (rowB > ( _board.length) || rowB <= 0){
-	          System.out.println ("That row does not exist. Try again.");
-	          rowB = Keyboard.readInt();}
-	
-	    System.out.println ("\nPlease indicate the column of the second element.");
-	    int columnB = Keyboard.readInt();
-	    while (columnB > ( _board[0].length) || columnB <= 0){
-	          System.out.println ("That column does not exist. Try again.");
-	         columnB = Keyboard.readInt();}
-	    _board[rowB-1][columnB-1].flip();
-	    System.out.print(this);
-
-	    if ((rowA == rowB) && (columnA == columnB)){
-		    System.out.println ("You chose the same element twice. Try again.");}
-
-	    else if (_board[rowA-1][columnA-1].equals (_board[rowB-1][columnB-1])){
-		score += 1;
-		System.out.println ("Correct!");}
-	    else {
-		 _board[rowA-1][columnA-1].flip();
-		 _board[rowB-1][columnB-1].flip(); 
-		 System.out.println ("Wrong.");}
-	    System.out.println (this);}
-	System.out.println ("Game Over.");
-		
-	    
-
-    }
-	
-	
-	
-
     //DO NOT MODIFY main()
     public static void main(String[] args){
 	Concentration game = new Concentration();
-	//System.out.print(game);
-	game.play();
+	System.out.print(game);
+	System.out.println(game.play());
+	//game.play();
     }
 
 }//end class Concentration
