ecddea3b616b1dc6346555e19198d1c6b40571da
goyyapazham <nsharadjaya@stuy.edu>
2015-11-24 14:40:13 -0500

    refreshes board

--- Concentration.java
+++ Concentration.java
@@ -15,8 +15,6 @@ public class Concentration {
     private String[] _words;     //list of unique Strings used for Tile vals
     private static int _numRows = 4;
 
-    //insert constructor and methods here
-
     public Concentration() {
 	
 	_numberFaceUp = 0;
@@ -26,13 +24,15 @@ public class Concentration {
     }
 
     public Tile[][] setBoard() {
+	//populates _board array
 	for(int r = 0; r < 4; r++) {
 	    for(int c = 0; c < 4; c++) {
 		Tile t = new Tile (_words[(4 * r + c) % 8]);
 		_board[r][c] = t ;
 	    }
 	}
-	
+
+	//randomizes _board array
 	for(int r = 0; r < 4; r++) {
 	    for(int c = 0; c < 4; c++) {
 		int rand1 = (int)(Math.random() * 4);
@@ -42,7 +42,6 @@ public class Concentration {
 	}
 	
 	return _board;
-
     }
 
     public static void swap(Object[][] a, int r1, int r2, int c1, int c2) {
@@ -52,6 +51,7 @@ public class Concentration {
 	a[r2][c2] = o1;
     }
 
+    //gets input
     public static int getSomething(String query) {
 	String error = "That wasn't a digit between 1-4. Please try again.";
 	System.out.println(query);
@@ -63,6 +63,7 @@ public class Concentration {
 	return i;
     }
 
+    //sees if game is over
     public boolean allUp() {
 	for(int r = 0; r < 4; r++) {
 	    for(int c = 0; c < 4; c++) {
@@ -73,28 +74,47 @@ public class Concentration {
     }
 
     public String play() {
+	//update
+	System.out.println("Here is the board:");
+	System.out.println(toString());
+
+	//gets values
 	int r1 = getSomething("First row (digit between 1-4):");
 	int c1 = getSomething("First column (digit between 1-4):");
 	int r2 = getSomething("Second row (digit between 1-4):");
 	int c2 = getSomething("Second column (digit between 1-4):");
+
+	//tests to see if the same card was flipped up twice
 	if ( r1 == r2 && c1 == c2 ) {
 	    System.out.println("You flipped the same cards! Try again:");
 	    play();
 	}
+
+	//if not, initialize tiles to flip
         Tile t1 = _board[r1][c1];
 	Tile t2 = _board[r2][c2];
+
+	//if one is already flipped, start over
 	if ( t1.isFaceUp() || t2.isFaceUp() ) {
 	    System.out.println("One or more of those cards was already flipped up! Try again:");
 	    play();
 	}
+
+	//if not, flip
 	t1.flip();
 	t2.flip();
+
+	//while the game isn't over
         while (!allUp()) {
+	    
+	    //if they're equal, it's a match
 	    if (t1.equals(t2)) {
 		System.out.print(toString());
 		System.out.println("That's a match! Try again:");
 		play();
 	    }
+
+	    //if not, flip them back down and try again
 	    else {
 		System.out.print(toString());
 		System.out.println("That wasn't a match! Try again:");
@@ -106,6 +126,7 @@ public class Concentration {
 	return "";
     }
 
+    //outputs board
     public String toString() {
 	String retBoard = "";
 	for(int r = 0; r < 4; r++) {
@@ -120,7 +141,6 @@ public class Concentration {
     //DO NOT MODIFY main()
     public static void main(String[] args){
 	Concentration game = new Concentration();
-	System.out.print(game);
 	game.play();
     }
 
